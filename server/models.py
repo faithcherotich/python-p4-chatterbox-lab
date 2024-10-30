f44c579c3d26ab06a08f063cc509cf4bf317781e
faithcherotich <faithcherotich910@gmail.com>
2024-10-30 16:40:41 +0300

    commit

--- server/models.py
+++ server/models.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
 from datetime import datetime
-
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -17,14 +15,5 @@ class Message(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'body': self.body,
-            'username': self.username,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at
-        }
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
